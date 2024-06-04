24391a470aab489bb5c173cfbe2f7e315cf7d34f
Mihajatiana <miandriakotonerason@gmail.com>
2024-06-04 17:53:18 +0300

    add: Header component

---
+++ app/_components/Header.tsx
@@ -0,0 +1,38 @@
+import { buttonVariants } from "@/components/ui/button";
+import { Section } from "./Section";
+import { GithubIcon } from "./icons/GithubIcon";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { GmailIcon } from "./icons/GmailIcon";
+import { LinkedInIcon } from "./icons/LinkedInIcon";
+
+export const Header = () => {
+  return (
+    <header className="sticky top-0 py-4">
+      <Section className="flex items-baseline">
+        <h1 className="text-lg font-bold text-primary">Mihajatiana</h1>
+        <div className="flex-1" />
+        <ul className="flex items-center gap-2">
+          <Link
+            className={cn(buttonVariants({ variant: "outline" }), "size-6 p-0")}
+            href={"#"}
+          >
+            <GithubIcon size={16} className="text-foreground" />
+          </Link>
+          <Link
+            className={cn(buttonVariants({ variant: "outline" }), "size-6 p-0")}
+            href={"#"}
+          >
+            <GmailIcon size={16} className="text-foreground" />
+          </Link>
+          <Link
+            className={cn(buttonVariants({ variant: "outline" }), "size-6 p-0")}
+            href={"#"}
+          >
+            <LinkedInIcon size={16} className="text-foreground" />
+          </Link>
+        </ul>
+      </Section>
+    </header>
+  );
+};
