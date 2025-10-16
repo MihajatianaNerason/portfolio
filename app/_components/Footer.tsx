259a88a3201b405da565ba0d4bae043333a0036d
Mihajatiana <miandriakotonerason@gmail.com>
2025-10-16 21:40:42 +0300

    fix: update copyright year in footer

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,11 +1,13 @@
 import { Section } from "./Section";
 
 export const Footer = () => {
+  const year = new Date().getFullYear();
+
   return (
     <footer className="bg-card">
       <Section className="py-8">
         <p className="text-muted-foreground text-sm text-center">
-          &copy;Copywrite 2024 Mihajatiana Andriakotonerason
+          &copy;Copywrite {year} Mihajatiana Andriakotonerason
         </p>
       </Section>
     </footer>
